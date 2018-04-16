f33d49e9aa050ea8e5825502583ead5bd2ad89fb
Robert Gulewicz <nayael@wp.pl>
2018-04-16 13:04:02 +0200

    Add dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     context: path.resolve('./src'),
-    entry: './index.js',
+    entry: ['whatwg-fetch', 'babel-polyfill', './index.js'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
@@ -71,5 +71,12 @@ module.exports = {
             template: './index.html',
             favicon: './favicon.ico'
         }),
+        new CopyWebpackPlugin([
+            {
+                from: 'img',
+                to: 'img',
+                toType: 'dir'
+            }
+          ], { copyUnmodified: true, debug: true })
     ]
 };
